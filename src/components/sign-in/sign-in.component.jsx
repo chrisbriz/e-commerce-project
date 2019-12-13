aab0a380224b87a97d9d2f05eaf58c8cef840af2
chrisbriz <christopher.brizet@outlook.com>
2019-12-13 08:00:30 +0100

    Sign in form created

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({ email: '', password: '' })
+    }
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <div>Sign in with your email and password</div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type='email'
+                        name='email'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required
+                    />
+                    <label>Email</label>
+                    <input
+                        type='password'
+                        name='password'
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        required />
+                    <label>Password</label>
+
+                    <input type="submit" value='Submit form' />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignIn;
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.style.scss';
+
+const SignInAndSignUpPage = () => (
+    <SignIn className='sign-in-and-sign-up'>
+        SIGN IN
+    </SignIn>
+)
+
+export default SignInAndSignUpPage; 
