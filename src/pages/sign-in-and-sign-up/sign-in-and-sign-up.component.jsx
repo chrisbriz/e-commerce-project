603b91a3646f66210e837ac4478b1f413301c5fc
chrisbriz <christopher.brizet@outlook.com>
2019-12-17 04:11:15 +0100

    implemented storage of users to firebase database

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.style.scss';
 
 const SignInAndSignUpPage = () => (
-    <SignIn className='sign-in-and-sign-up'>
-        SIGN IN
-    </SignIn>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+        <SignUp />
+    </div>
 )
 
 export default SignInAndSignUpPage; 
